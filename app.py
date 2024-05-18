88157e1ca42132df3a5b4bb7537a73b4f90eeb10
Vikranth3140 <vikranth22570@iiitd.ac.in>
2024-05-19 03:09:47 +0530

    "Removed TextBlob sentiment analysis, added Chart.js for visualization, and refactored code to use only VADER sentiment analysis"

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from textblob import TextBlob
 import nltk
 
+# Download necessary NLTK data
 nltk.download('vader_lexicon')
 
 app = Flask(__name__)
@@ -13,31 +13,15 @@ def index():
 
 @app.route('/process', methods=['POST'])
 def process():
-    text = request.form['inputText']
+    input_text = request.form['inputText']
     option = request.form['option']
-
+    
     if option == 'Sentiment':
-        sentiment_scores = analyze_sentiment(text)
-        return render_template('index.html', sentiment_scores=sentiment_scores)
-    else:
-        return render_template('index.html', error_message="Invalid option selected")
-
-def analyze_sentiment(text):
-    # Using VADER sentiment analysis
-    sia = SentimentIntensityAnalyzer()
-    vader_scores = sia.polarity_scores(text)
-
-    # Using TextBlob sentiment analysis
-    blob = TextBlob(text)
-    textblob_sentiment = blob.sentiment
-
-    return {
-        'vader': vader_scores,
-        'textblob': {
-            'polarity': textblob_sentiment.polarity,
-            'subjectivity': textblob_sentiment.subjectivity
-        }
-    }
+        sid = SentimentIntensityAnalyzer()
+        sentiment_scores = sid.polarity_scores(input_text)
+        return render_template('index.html', sentiment_scores=sentiment_scores, input_text=input_text)
+    
+    return render_template('index.html', error_message="Invalid option selected.")
 
 if __name__ == '__main__':
     app.run(debug=True)
